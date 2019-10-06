d99cefdd9dfd25a3f37a1e9f2cddcadc2d170751
wenzhe <mysteryven@gmail.com>
2019-10-06 17:38:47 +0800

    feat:linkedlist

--- linkedList/linkedList.ts
+++ linkedList/linkedList.ts
@@ -1,19 +1,52 @@
 // 链表
-interface Node {
+interface LinkedNode {
   value: any;
-  next: Object;
+  next: LinkedNode | null;
 }
 
 interface LinkedList {
-  insert: () => void;
-  get: () => number;
+  insert: (val: any) => void;
+  find: (val: any) => LinkedNode | boolean;
+  get: () => LinkedNode;
+}
+
+function generatorNode(val: any): LinkedNode {
+  return {
+    value: val,
+    next: null,
+  }
 }
 
 function linkedList(): Function {
+  let head = generatorNode('');
+  let current = head;
+
   return function inner(): LinkedList {
     return {
-      insert: () => { },
-      get: () => 1
+      insert,
+      find,
+      get
+    }
+
+    function insert(val: any) {
+      const newNode = generatorNode(val);
+      current.next = newNode;
+      current = newNode;
+    }
+
+    function get() {
+      return head
+    }
+
+    function find(val: any) {
+      let target: LinkedNode = head.next;
+      while (target !== null && target.value !== val) {
+        target = target.next;
+      }
+      if (target === null) {
+        return false;
+      }
+      return target;
     }
   }
 }
--- linkedList/test/linkedList.test.ts
+++ linkedList/test/linkedList.test.ts
@@ -1,8 +1,28 @@
 let nodes = require('../linkedList.ts');
 
-describe('reverseLinkedList', () => {
-  it('return 1', () => {
-    let nodes1 = nodes();
-    expect(nodes1.get()).toBe(1);
+describe('LinkedList', () => {
+  let nodesExample = nodes();
+
+  beforeEach(() => {
+    nodesExample.insert(1);
+    nodesExample.insert('hello');
   })
-})
+
+  it('can insert in right position', () => {
+    expect(nodesExample.get()).toEqual({
+      value: '',
+      next: {
+        value: 1,
+        next: {
+          value: 'hello',
+          next: null
+        },
+      }
+    });
+  })
+
+  it('can find right item with param, and return `false` if not find', () => {
+    expect(nodesExample.find(1).value).toBe(1);
+    expect(nodesExample.find('hello').value).toBe('hello');
+  })
+});
