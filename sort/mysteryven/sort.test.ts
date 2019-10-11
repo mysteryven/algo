ca20bea950d7f46d33f67b7a2f4678281ac5085e
wenzhe <mysteryven@gmail.com>
2019-10-11 14:25:07 +0800

    feat:mergeSort

--- sort/mysteryven/sort.test.ts
+++ sort/mysteryven/sort.test.ts
@@ -1,4 +1,4 @@
-import { insertSort } from './sort';
+import { insertSort, mergeSort, merge } from './sort';
 
 const res = [1, 2, 3, 4, 5];
 
@@ -6,26 +6,55 @@ describe('insertSort', () => {
   testGroup(insertSort)
 })
 
+describe('mergeSort', () => {
+  testGroup(mergeSort)
+})
+
+describe('merge function', () => {
+  it('can merge 1 element', () => {
+    let a = [2];
+    expect(merge(a, 0, 0, 0)).toEqual([2])
+  })
+  it('can merge 2 element', () => {
+    let a = [2, 1];
+    expect(merge(a, 0, 0, 1)).toEqual([1, 2])
+  })
+  it('can merge 3 element', () => {
+    let a = [1, 3, 2];
+    expect(merge(a, 0, 1, 2)).toEqual([1, 2, 3])
+  })
+  it('can merge 6 element', () => {
+    let a = [1, 2, 3, 0, 5, 9];
+    expect(merge(a, 0, 2, 5)).toEqual([0, 1, 2, 3, 5, 9])
+  })
+})
+
 
 function testGroup(fun) {
   it('works when arr length is 1', () => {
     const a = [1];
-    fun(a);
-    expect(a).toEqual([1])
+    const b = fun(a);
+    expect(b).toEqual([1])
   })
   it('works when arr is reverse', () => {
     const a = [5, 4, 3, 2, 1];
-    fun(a);
-    expect(a).toEqual(res)
+    commonExpect(a)
   })
   it('works when arr is ordered', () => {
     const a = [1, 2, 3, 4, 5];
-    fun(a);
-    expect(a).toEqual(res)
+    commonExpect(a)
   })
   it('works when arr is in random order', () => {
-    const a = [3, 2, 1, 4, 5];
-    fun(a);
-    expect(a).toEqual(res)
+    const a1 = [1, 2, 3, 0, 5, 9];
+    const a2 = [1, 2, 0, 5, 9];
+    const b1 = fun(a1);
+    const b2 = fun(a2);
+    expect(b1).toEqual([0, 1, 2, 3, 5, 9])
+    expect(b2).toEqual([0, 1, 2, 5, 9])
   })
+  function commonExpect(a) {
+    const b = fun(a);
+    expect(b).toEqual(res)
+  }
 }
+
--- sort/mysteryven/sort.ts
+++ sort/mysteryven/sort.ts
@@ -1,6 +1,6 @@
 type ArrType = string[] | number[]
 
-function exch(index1:number, index2:number, arr: ArrType) {
+function exch(index1: number, index2: number, arr: ArrType) {
   let temp = arr[index1];
   arr[index1] = arr[index2];
   arr[index2] = temp;
@@ -10,17 +10,73 @@ function insertSort(arr: ArrType) {
   const length = arr.length;
   if (length === 1) {
     return arr;
-  } 
+  }
   for (let i = 1; i < length; i++) {
     let j = i;
-    while(arr[j-1] > arr[j]) {
-      exch(j, j-1, arr);
+    while (arr[j - 1] > arr[j]) {
+      exch(j, j - 1, arr);
       j--;
     }
   }
   return arr;
 }
 
+function mergeSort(arr: ArrType) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  let res = mergeSort_c(arr, 0, arr.length - 1);
+  return res;
+
+  function mergeSort_c(arr: ArrType, lo: number, hi: number) {
+    if (hi <= lo) {
+      return;
+    }
+    let mid = lo + Math.floor((hi - lo) / 2);
+    mergeSort_c(arr, lo, mid)
+    mergeSort_c(arr, mid + 1, hi)
+    return merge(arr, lo, mid, hi);
+  }
+}
+
+
+function merge(arr, lo, mid, hi) {
+  const temp = [];
+  let tempIndex = 0;
+  let i = lo;
+  let j = Math.min(mid + 1, hi);
+
+  while (i <= mid && j <= hi) {
+    if (arr[i] > arr[j]) {
+      temp[tempIndex] = arr[j];
+      j++;
+    } else {
+      temp[tempIndex] = arr[i];
+      i++
+    }
+    tempIndex++
+  }
+
+  while (i <= mid) {
+    temp[tempIndex] = arr[i];
+    tempIndex++
+    i++
+  }
+  while (j <= hi) {
+    temp[tempIndex] = arr[j];
+    j++
+    tempIndex++
+  }
+  for (let i = lo; i <= hi; i++) {
+    arr[i] = temp[i - lo];
+  }
+  return arr;
+}
+
+
 export {
-  insertSort
+  insertSort,
+  mergeSort,
+  merge,
 }
