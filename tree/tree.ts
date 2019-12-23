3721d784880c1e9949e95db8a3e345a8fde6fed0
王文哲 <mysteryven@gmail.com>
2019-12-23 15:48:24 +0800

    feat:basic tree

---
+++ tree/tree.ts
@@ -0,0 +1,47 @@
+
+interface TreeNode {
+  left: TreeNode | null;
+  right: TreeNode | null;
+  data: number;
+}
+
+function gTreeNode(num: number): TreeNode {
+  return {
+    left: null,
+    right: null,
+    data: num
+  }
+}
+
+class Tree {
+  private root: TreeNode | null = null;
+
+  insert(data: number) {
+    if (this.root === null) {
+      this.root = gTreeNode(data);
+    } else {
+      let p:TreeNode = this.root;
+      while(true) {
+        if (data < p.data) {
+          if (p.left) {
+            p = p.left;
+          } else {
+            p.left = gTreeNode(data)
+            break;
+          }
+        } else {
+          if (p.right) {
+            p = p.right;
+          } else {
+            p.right = gTreeNode(data)
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  get Tree() {
+    return this.root;
+  }
+}
